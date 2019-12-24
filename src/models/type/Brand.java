ce225da9c98bcf8b057627a0c0de19511e1deafc
Rezinford <rezinford777@gmail.com>
2019-12-25 00:58:57 +0200

    Initial commit

---
+++ src/models/type/Brand.java
@@ -0,0 +1,10 @@
+package models.type;
+
+import java.lang.reflect.Parameter;
+import java.util.List;
+
+public class Brand {
+    private String name;
+    private List<Parameter> specification;
+
+}
